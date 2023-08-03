c2b13036f925b19736bb6d96b662c0865d42cb39
Claudio Vargas <Claudio.g.vargas@gmail.com>
2023-08-03 07:55:24 -0300

    Criada as classes: Accont, BusinessAccont e Program

---
+++ src/aplication/Program.java
@@ -0,0 +1,21 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Account accont = new Account();
+		
+		
+		
+		sc.close();
+
+	}
+
+}
